9c102edcc9b9752814fb8d10671fa6fbd7905e87
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-06-11 11:47:44 +0330

    Add UserCard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,23 @@
+import UserCard from "@/components/UserCard";
 import React from "react";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className="p-4 flex flex-col md:flex-row gap-4">
+      {/* Left */}
+      <div className="w-full lg:w-2/3">
+        {/* User Cards */}
+        <div className=" flex justify-between gap-4 flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  );
 };
 
 export default Admin;
