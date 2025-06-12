864f314e84a63fb5f9985658b57c8987f24a7989
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-06-12 11:03:06 +0330

    Finished degine Parent page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
 
 const Parent = () => {
-  return <div>Parent</div>;
+  return (
+    <div className="flex-1 p-4 flex flex-col md:flex-row gap-4">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-xl p-4">
+          <h1 className="text-xl font-semibold">Schedule (Mehdi Shirani)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default Parent;
