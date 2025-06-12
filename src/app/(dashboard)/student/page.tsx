32a7a5f81f89d30c3931b65f3eac86df4fc826ab
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-06-12 10:29:10 +0330

    Add the BigCalendar component

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,25 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
 import React from "react";
 
 const Student = () => {
-  return <div>Student</div>;
+  return (
+    <div className="p-4 flex flex-col md:flex-row gap-4">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-xl p-4">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default Student;
